409e9512b9c99e87329b2b692410362abeac64f4
Gabriel Topp <20438377+Fizztopp@users.noreply.github.com>
2019-11-21 10:38:40 +0100

    added copyright statement

--- SSH.cpp
+++ SSH.cpp
@@ -1,7 +1,24 @@
 /**
- *	TIGHT-BINDING MODEL FOR SSH MODEL (TBG)
- *  (c) Gabriel E. Topp, gabriel.topp@mpsd.mpg.de
- * 	
+ *
+ * Copyright (C) 2019 Gabriel E. Topp
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ * 
+ * TIGHT-BINDING MODEL FOR SSH MODEL (TBG)
  * This code allows a selfconsistent calculation of the temperature dependent equilibrium displacement u aswell as the Peierls-undiced electronic dynamics + nuclear Ehrenfest synamcis  
  *  
  * Output:
